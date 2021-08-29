f961994b1e697302b096ca3327d338cea01466b0
Briceyda <Brd0208@gmail.com>
2021-08-29 00:10:51 -0500

    users and product schema

---
+++ database/db.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE bq_database;
+
+DROP DATABASE IF EXISTS bq_database;
+
+CREATE DATABASE bq_database;
+
+USE bq_database;
+
+-- USERS TABLE
+CREATE TABLE users(
+  id INT(11) NOT NULL,
+  email VARCHAR(16) NOT NULL,
+  password VARCHAR(60) NOT NULL,
+  role TINYINT default 0 NOT NULL
+);
+
+ALTER TABLE users
+    ADD PRIMARY KEY(id);
+
+ALTER TABLE users
+    MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+
+DESCRIBE users;
+
+-- PRODUCT TABLE
+CREATE TABLE product(
+  id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(20) NOT NULL,
+  price FLOAT(4,2) NOT NULL ,
+  image VARCHAR(200) NULL,
+  type VARCHAR(20) NOT NULL,
+  dateEntry DATE NOT NULL,
+)
+
+
+-- ORDER TABLE
