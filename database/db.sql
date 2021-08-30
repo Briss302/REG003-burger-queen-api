6cf0a1194166f80f15b857494d18b8816476dc09
Briceyda <Brd0208@gmail.com>
2021-08-30 13:27:11 -0500

    order schema

--- database/db.sql
+++ database/db.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE bq_database;
-
 DROP DATABASE IF EXISTS bq_database;
 
 CREATE DATABASE bq_database;
@@ -26,11 +24,20 @@ DESCRIBE users;
 CREATE TABLE product(
   id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(20) NOT NULL,
-  price FLOAT(4,2) NOT NULL ,
+  price FLOAT(4,2) NOT NULL,
   image VARCHAR(200) NULL,
   type VARCHAR(20) NOT NULL,
-  dateEntry DATE NOT NULL,
+  dateEntry timestamp NOT NULL default current_timestamp,
+  
 )
 
-
 -- ORDER TABLE
+CREATE TABLE order(
+  id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user_id INT(11),
+  client VARCHAR(20) NOT NULL,
+  status VARCHAR(20) default 'pending' NOT NULL,
+  dateEntry timestamp NOT NULL default current_timestamp,
+  product
+  CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
+)
